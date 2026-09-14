c620659a59e4c217be80fa4e738716c4992c3ae7
Noxai8 <jackslow555@gmail.com>
2026-09-15 01:04:42 +0200

    Update NoxLogo.tsx

--- src/pages/src/components/NoxLogo.tsx
+++ src/pages/src/components/NoxLogo.tsx
@@ -32,36 +32,20 @@ export default function NoxLogo({
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
+        focusable="false"
         style={{
           display: 'block',
           overflow: 'visible',
+          flexShrink: 0,
         }}
       >
         <path
-          d="
-            M10.8 8.7
-            C12.4 5.9 16 4.8 18.9 6.2
-            L31.8 12.4
-            C35 13.9 36.2 17.7 34.6 20.7
-            C33 23.6 29.4 24.7 26.4 23.3
-            L13.5 17.1
-            C10.4 15.6 9.2 11.8 10.8 8.7
-            Z
-          "
+          d="M10.8 8.7C12.4 5.9 16 4.8 18.9 6.2L31.8 12.4C35 13.9 36.2 17.7 34.6 20.7C33 23.6 29.4 24.7 26.4 23.3L13.5 17.1C10.4 15.6 9.2 11.8 10.8 8.7Z"
           fill="currentColor"
         />
 
         <path
-          d="
-            M27.4 29.7
-            C28.1 26.4 31.2 24.2 34.5 24.7
-            L61.3 28.7
-            C64.8 29.2 67.1 32.4 66.5 35.8
-            C66 39.1 62.9 41.4 59.5 40.9
-            L32.7 36.9
-            C29.3 36.4 26.9 33.2 27.4 29.7
-            Z
-          "
+          d="M27.4 29.7C28.1 26.4 31.2 24.2 34.5 24.7L61.3 28.7C64.8 29.2 67.1 32.4 66.5 35.8C66 39.1 62.9 41.4 59.5 40.9L32.7 36.9C29.3 36.4 26.9 33.2 27.4 29.7Z"
           fill="currentColor"
         />
       </svg>
@@ -74,6 +58,7 @@ export default function NoxLogo({
             gap: 5,
             lineHeight: 1,
             color,
+            whiteSpace: 'nowrap',
           }}
         >
           <span
